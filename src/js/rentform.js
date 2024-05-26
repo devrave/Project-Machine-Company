2ffed735c23b1e1b416b92e211a27572db5c0ba7
andres1988 <a.canorave@gmail.com>
2024-05-26 12:04:34 -0500

    organización de estructura de carpetas

---
+++ src/js/rentform.js
@@ -0,0 +1,22 @@
+
+const btn = document.getElementById('button');
+
+let formulario = document.getElementById('form')
+  formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Enviando...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_i7ofdu8';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'ENVIAR MENSAJE';
+        alert('Mensaje enviado correctamente!');
+        formulario.reset()
+      }, (err) => {
+        btn.value = 'ENVIAR MENSAJE';
+        alert(JSON.stringify(err));
+      });
+  });
